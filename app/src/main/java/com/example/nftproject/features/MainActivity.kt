c54fecb58ab3ad5eb71abd6ca7f1ef65f200d238
alstlr0312 <alstlr0312@naver.com>
2023-08-18 17:02:58 +0900

    bottom_nav 수정

--- app/src/main/java/com/example/nftproject/features/MainActivity.kt
+++ app/src/main/java/com/example/nftproject/features/MainActivity.kt
@@ -2,28 +2,39 @@ package com.example.nftproject.features
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.nftproject.R
 import com.example.nftproject.databinding.ActivityMainBinding
+import com.example.nftproject.features.exchange.ExchangeFragment
+import com.example.nftproject.features.gift.GiftFragment
+import com.example.nftproject.features.home.HomeFragment
+import com.example.nftproject.features.mypage.MypageFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        initBinding()
-        initNavigation()
-    }
+        bottomNavigationView = findViewById(R.id.nav_bar)
 
-    private fun initBinding() {
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
+        // 처음 화면
+        supportFragmentManager.beginTransaction().add(R.id.main_frame, HomeFragment()).commit()
 
-    private fun initNavigation() {
-        NavigationUI.setupWithNavController(binding.navBar, findNavController(R.id.nav_host))
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem: MenuItem ->
+            when (menuItem.itemId) {
+                R.id.homeFragment -> supportFragmentManager.beginTransaction().replace(R.id.main_frame, HomeFragment()).commit()
+                R.id.exchangeFragment -> supportFragmentManager.beginTransaction().replace(R.id.main_frame, ExchangeFragment()).commit()
+                R.id.giftFragment -> supportFragmentManager.beginTransaction().replace(R.id.main_frame, GiftFragment()).commit()
+                R.id.mypageFragment -> supportFragmentManager.beginTransaction().replace(R.id.main_frame, MypageFragment()).commit()
+
+            }
+            true
+        }
     }
 }
--- app/src/main/java/com/example/nftproject/features/login/LoginActivity.kt
+++ app/src/main/java/com/example/nftproject/features/login/LoginActivity.kt
@@ -1,12 +1,23 @@
 package com.example.nftproject.features.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 import com.example.nftproject.R
+import com.example.nftproject.databinding.ActivityLoginBinding
+import com.example.nftproject.features.MainActivity
+import com.example.nftproject.features.home.HomeFragment
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val intent = Intent(this, MainActivity::class.java)
+        binding.loginBtn.setOnClickListener{startActivity(intent)}
+
     }
 }
