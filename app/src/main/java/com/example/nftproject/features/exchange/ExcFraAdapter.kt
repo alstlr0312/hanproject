24ef7887bd5c36fe1d6e49f7f99900c296df1cbf
alstlr0312 <alstlr0312@naver.com>
2023-09-14 16:22:27 +0900

    교환 UI 생성 밑 다른 UI 수정+내가 발행한 NFT 확인

--- app/src/main/java/com/example/nftproject/features/exchange/ExcFraAdapter.kt
+++ app/src/main/java/com/example/nftproject/features/exchange/ExcFraAdapter.kt
@@ -1,4 +1,99 @@
 package com.example.nftproject.features.exchange
 
-class ExcFraAdapter {
+import android.content.Context
+import android.net.Uri
+import android.util.SparseBooleanArray
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.nftproject.databinding.ExchangeRvBinding
+import com.example.nftproject.model.movieListItem
+
+class ExcFraAdapter(private val context: Context) : RecyclerView.Adapter<ExcFraAdapter.ViewHolder>() {
+    var itemList = mutableListOf<movieListItem>()
+    var checkedItems = HashMap<movieListItem, Boolean>()  // Change to HashMap
+    private var listener: OnItemClickListener? = null
+
+    private var itemCheckedListener: OnItemCheckedListener? = null
+
+    fun setOnItemCheckedListener(listener: OnItemCheckedListener) {
+        itemCheckedListener = listener
+    }
+
+    inner class ViewHolder(private val binding: ExchangeRvBinding) : RecyclerView.ViewHolder(binding.root),
+        View.OnClickListener {
+        init {
+            itemView.setOnClickListener(this)
+            binding.checkBox.setOnCheckedChangeListener { _, isChecked ->
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    handleCheckboxClick(itemList[position], isChecked)
+                }
+            }
+        }
+
+        fun bind(item: movieListItem) {
+            Glide.with(itemView).load(Uri.parse(item.poster)).into(binding.rvMovieimg)
+            binding.rvTitle.text = item.movieTitle
+
+            // Set checkbox state
+            binding.checkBox.isChecked = checkedItems[item] ?: false  // Change to use the movieListItem as key instead of position.
+        }
+
+        override fun onClick(v: View?) {
+            val pos = adapterPosition
+            if (pos != RecyclerView.NO_POSITION) {
+                listener?.onItemClick(itemView, itemList[pos], pos)
+            }
+        }
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ExchangeRvBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(itemList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    // 체크박스 클릭 처리 메서드
+    private fun handleCheckboxClick(item: movieListItem, isChecked: Boolean) {
+        if (isChecked && checkedItems.values.count { it } >= 3 && !checkedItems.getOrDefault(item,false)) {
+            Toast.makeText(context, "이미 3개가 선택되어 있습니다", Toast.LENGTH_SHORT).show()
+            return
+        }
+        // Update selection status.
+        checkedItems[item] = isChecked
+        itemCheckedListener?.onItemChecked(item, isChecked)
+    }
+
+
+    // 아이템 추가 메서드
+    fun addItem(item: movieListItem){
+        itemList.add(item)
+        notifyItemInserted(itemCount-1)
+    }
+
+    // 모든 아이템 제거 메서드
+    fun removeAllItem(){
+        itemList.clear()
+        notifyDataSetChanged()
+    }
+
+    interface OnItemCheckedListener {
+        fun onItemChecked(item: movieListItem, isChecked: Boolean)
+    }
+    interface OnItemClickListener{
+        fun onItemClick(v: View, data: movieListItem, pos : Int)
+    }
 }
--- app/src/main/java/com/example/nftproject/features/exchange/ExchangeFragment.kt
+++ app/src/main/java/com/example/nftproject/features/exchange/ExchangeFragment.kt
@@ -1,23 +1,155 @@
 package com.example.nftproject.features.exchange
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.util.SparseBooleanArray
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import com.bumptech.glide.Glide
 import com.example.nftproject.R
+import com.example.nftproject.databinding.FragmentExchangeBinding
+import com.example.nftproject.features.home.HomeViewModel
+import com.example.nftproject.model.movieListItem
+import com.unity.mynativeapp.config.DialogFragment
+import java.util.*
 
 
-class ExchangeFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class ExchangeFragment: DialogFragment<FragmentExchangeBinding>(FragmentExchangeBinding::bind, R.layout.fragment_exchange) {
+    lateinit var exAdapter: ExcFraAdapter
+    private val viewModel by viewModels<HomeViewModel>()
+    private var getPostIsFirst = true
+    private var getPostHasNext = false
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setView()
+        subscribeUI()
+        searchMovie()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_exchange, container, false)
+    private fun setView() {
+        exAdapter = ExcFraAdapter(requireContext())
+        binding.rvPostList.adapter = exAdapter
+        val layoutManager = GridLayoutManager(requireContext(), 3)
+        binding.rvPostList.layoutManager = layoutManager
+        viewModel.getMovie()
+
+        exAdapter.setOnItemCheckedListener(object : ExcFraAdapter.OnItemCheckedListener {
+            override fun onItemChecked(item: movieListItem, isChecked: Boolean) {
+                if (isChecked) {
+                    // 아이템이 체크되었을 때의 동작 구현
+                    when {
+                        binding.chooseimg1.tag == null -> {
+                            binding.chooseimg1.tag = item.id
+                            Glide.with(requireContext()).load(Uri.parse(item.poster)).into(binding.chooseimg1)
+                        }
+                        binding.chooseimg2.tag == null -> {
+                            binding.chooseimg2.tag = item.id
+                            Glide.with(requireContext()).load(Uri.parse(item.poster)).into(binding.chooseimg2)
+                        }
+                        binding.chooseimg3.tag == null -> {
+                            binding.chooseimg3.tag = item.id
+                            Glide.with(requireContext()).load(Uri.parse(item.poster)).into(binding.chooseimg3)
+                        }
+                        else -> {
+                            return
+                        }
+                    }
+                } else {
+                    // 아이템이 해제되었을 때의 동작 구현
+                    val itemIdToRemove = item.id
+                    if (binding.chooseimg1.tag == itemIdToRemove) {
+                        binding.chooseimg1.setImageResource(R.drawable.input_img)
+                        binding.chooseimg1.tag = null // 태그 초기화
+                    } else if (binding.chooseimg2.tag == itemIdToRemove) {
+                        binding.chooseimg2.setImageResource(R.drawable.input_img)
+                        binding.chooseimg2.tag= null//태그초기화
+                    } else {
+                        binding.chooseimg3.setImageResource(R.drawable.input_img)
+                        binding.chooseimg3.tag = null//태그초기화
+                    }
+                }
+            }
+        })
     }
 
+    private fun searchMovie() {
+        binding.searchtext.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // No implementation needed
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                // No implementation needed
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                val searchText = s.toString().toLowerCase(Locale.getDefault())
+
+                val originalList = viewModel.myMovieData.value?.movieListDtos ?: listOf()
+
+                exAdapter.itemList = if (searchText.isEmpty()) {
+                    originalList.toMutableList()
+                } else {
+                    originalList.filter { data ->
+                        val movieName = data.movieTitle?.toLowerCase(Locale.getDefault())
+                        movieName != null && movieName.contains(searchText)
+                    }.toMutableList()
+                }
+
+                exAdapter.notifyDataSetChanged()
+            }
+        })
+    }
+
+
+
+    private fun subscribeUI() {
+        viewModel.toastMessage.observe(viewLifecycleOwner) { message ->
+            Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
+        }
+
+        viewModel.loading.observe(viewLifecycleOwner) { isLoading ->
+            //    if (isLoading) showLoadingDialog(requireContext()) else dismissLoadingDialog()
+        }
+        viewModel.logout.observe(this) {
+            if (it) logout()
+        }
+
+        viewModel.myMovieData.observe(viewLifecycleOwner) { data ->
+            Log.d("Check", "Data: $data")
+            Log.d("있음", "있음")
+            if (data != null) {
+                Log.d("Check", "nftListDto: ${data.movieListDtos}")
+                Log.d("있음", "있음")
+                if (data.movieListDtos != null) {
+                    exAdapter.removeAllItem()
+                    val getMovieInfo = data.movieListDtos
+                    if (getMovieInfo.isEmpty()) {
+                        binding.rvPostList.visibility = View.INVISIBLE
+                        Log.d("있음", "있음")
+                    } else {
+                        binding.rvPostList.visibility = View.VISIBLE
+                        Log.d("있음", "있음")
+                    }
+                    for (pnft in getMovieInfo) {
+                        exAdapter.addItem(
+                            movieListItem(
+                                id = pnft.id,
+                                movieTitle = pnft.movieTitle,
+                                poster = pnft.poster
+                            )
+                        )
+                    }
+                    getPostHasNext = data.hasNext
+                    getPostIsFirst = data.isFirst
+                }
+            }
+        }
+    }
 }
